1858381cf86c596d65309cc0b2a51331c8342475
davidsunday <sundaydavid989@gmail.com>
2025-04-06 16:18:32 +0100

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
+    id("jacoco")
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
     namespace = "com.golvia.smartflowtechandroidassessment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.golvia.smartflowtechandroidassessment"
@@ -56,4 +60,75 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Lifecycle & ViewModel
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Retrofit, OkHttp & Gson/Moshi (for network requests)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Coroutine support for retrofit calls
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Logging Interceptor (OkHttp)
+    implementation(libs.logging.interceptor)
+    // Dagger Hilt
+    implementation (libs.hilt.dep )
+    kapt(libs.hilt.complier)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+
+    // coil
+    implementation(libs.accompanist.coil)
+    implementation(libs.coil.compose)
+    testImplementation(libs.mockk)
+
+    testImplementation(libs.mockk.agent.jvm)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.withType<Test> {
+    configure<JacocoTaskExtension> {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest", "createDebugCoverageReport")
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    val fileFilter = listOf(
+        "**/R.class",
+        "**/R$*.class",
+        "**/BuildConfig.*",
+        "**/Manifest*.*",
+        "**/*Test*.*",
+        "android/**/*.*"
+    )
+
+    val debugTree = fileTree("${project.buildDir}/intermediates/classes/debug") {
+        exclude(fileFilter)
+    }
+
+    val mainSrc = "${project.projectDir}/src/main/java"
+
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(files(debugTree))
+    executionData.setFrom(fileTree(project.buildDir) {
+        include(
+            "jacoco/testDebugUnitTest.exec",
+            "outputs/code-coverage/connected/*coverage.ec"
+        )
+    })}
